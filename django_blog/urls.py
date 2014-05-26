8ac592dfaa0e4f25e7682efa8720bd4d7bb784cc
Shane Burke <shaneob121@hotmail.com>
2014-05-26 14:57:07 +0100

    Implemented flat page support

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -5,12 +5,15 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'django_blog.views.home', name='home'),
+    # url(r'^$', 'django_tutorial_blog_ng.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 
     # Blog URLs
     url(r'', include('blogengine.urls')),
+
+    # Flat pages
+    url(r'', include('django.contrib.flatpages.urls')),
 )
 
